0a34eb8a6e05ba91e5df2f6a72d5412323da2c9f
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-26 11:17:43 +0300

    setting files

---
+++ src/components/typing.js
@@ -0,0 +1,13 @@
+import '../styles/typing.css'
+
+const TypingTester = () => {
+    return(
+        <div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default TypingTester
