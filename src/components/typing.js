619d85020bec4cfb20000b5c621c8be245de802e
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-26 11:22:43 +0300

    seperating the array or pragrapg

--- src/components/typing.js
+++ src/components/typing.js
@@ -3,10 +3,18 @@ import '../styles/typing.css'
 const TypingTester = () => {
 
     const paragraph = `The golden sun dipped behind the hills, casting a warm glow across the open fields. A gentle breeze stirred the tall grass, making it dance in waves like a green ocean. Birds soared high above, their songs filling the evening air with sweet melodies. Somewhere in the distance, a river murmured softly as it wound its way through the valley, reflecting the last light of day. It was a moment of peace, a reminder that even in a busy world, nature always finds a way to slow things down.`
+
+
     return(
         <div className='main-div'>
             <div className='test'>
-
+                {
+                    paragraph.split("").map((char, index) =>{
+                        <span className='char'> 
+                            {char}
+                        </span>
+                    })
+                }
             </div>
             <div className='result'>
                 <p>Time Left</p>
